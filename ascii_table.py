99f2d65825832a4f5af5cca82064928ed50bab84
KayZinPhone <44814583+KayZinPhone@users.noreply.github.com>
2018-11-27 01:48:48 +0800

    Ascii_Table with function

---
+++ ascii_table.py
@@ -0,0 +1,53 @@
+""" value = input("Enter a character: ")
+
+ code = ord(value)
+ print("the ASCII code for {} is {}".format(value,code))
+
+ num = int(input("Enter a number between 33 and 127: "))
+ character = chr(num)
+ print("The character for {} is {}".format(num, character))"""
+
+
+"""MIN_VALUE = 33
+MAX_VALUE =127
+
+value = input("Enter a character: ")
+
+code = ord(value)
+print("the ASCII code for {} is {}".format(value,code))
+
+num = int(input("Enter a number between 33 and 127: "))
+character = ""
+while not MIN_VALUE < num < MAX_VALUE:
+    print("Please enter between {} and {}".format(MIN_VALUE, MAX_VALUE))
+    num = int(input("Enter a number between 33 and 127: "))
+
+character = chr(num)
+print("The character for {} is {}".format(num, character))
+
+"""
+"""
+
+MIN_VALUE = 33
+MAX_VALUE = 127
+for i in range(MIN_VALUE, MAX_VALUE+1):
+    print("{}{:>2s}".format(i, chr(i)))
+"""
+
+MIN_VALUE = int(input("Enter a minimum number: "))
+MAX_VALUE = int(input("Enter a maximum number: "))
+var = int(input("Enter a number between {} and {}: ".format(MIN_VALUE, MAX_VALUE)))
+
+
+def get_number(MIN_VALUE, MAX_VALUE, num):
+    try:
+        while not MIN_VALUE < num < MAX_VALUE:
+            print("Please enter a valid number")
+            num = int(input("Enter a number between {} and {}: ".format(MIN_VALUE, MAX_VALUE)))
+    except ValueError:
+        print("Please enter a valid number")
+        num = int(input("Enter a number between {} and {}: ".format(MIN_VALUE, MAX_VALUE)))
+
+    return num
+
+print(get_number(MIN_VALUE, MAX_VALUE, var))
